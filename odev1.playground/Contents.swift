5321034037c04a392f01b0e58a2c71542ab95afb
“Mert-digi” <mert@digiterra.com.tr>
2022-09-21 17:32:11 +0300

    Odev1

---
+++ odev1.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//islem
+var ogrno : Int = 77;
+var kitapno : Int = 30;
+var atarih : String = "05/04/2010"
+var vtarih : String = "01/05/2010"
+
+//ogr
+var ograd = "Mert"
+var ogrsoyad = "Atağ"
+var cinsiyet = "Erkek"
+var sinif = 12
+var dtarih="06/04/2000"
+//
+
+//kitap
+var kitapadi = "Sefiller"
+var yazarno = 1
+var turno = 1
+var sayfasayisi = 750
+var puan = 8.3
+
+//yazar
+var yazarad = "Victor"
+var yazarsoyad = "Hugo"
+
+//tur
+var turadi = "Roman"
+
+
